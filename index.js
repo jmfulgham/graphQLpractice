e4a0cad2d46936d53919dbc59c20321bf19de68a
Mone't Fulgham <monetfulgham@gmail.com>
2018-06-22 12:40:28 -0400

    Setting up types, queries, and mutations

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-// GraphQL Server Goes Here!
+const {GraphQLServer} = require('graphql-yoga');
+const photos = require('./data/photos.json');
+const users = require('./data/users.json');
+const typeDefs = `
+    type Photo{
+        id: ID!
+        name: String
+        description: String
+        category: PhotoCategory
+    }
+
+    type User{
+        id: ID!
+        name: String!
+    }
+    enum PhotoCategory {
+        LANDSCAPE
+        SELFIE
+        PORTRAIT
+    }
+
+    type Query{
+        totalPhotos: Int!
+        allPhotos: [Photo!]!
+        totalUsers: Int!
+        allUsers: [User!]!
+    }
+
+    type Mutation{
+        postPhoto(input:PostPhotoInput!): Photo!
+
+    }
+
+    input PostPhotoInput{
+        name: String!
+        description: String
+        category: PhotoCategory=PORTRAIT
+    }
+
+`
+const resolvers = {
+    Query : {
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos,
+        totalUsers: () => user.length,
+        allUsers: ()=> user
+    },
+
+
+    Mutation: {
+        postPhoto: (root, args ) => {
+            const newPhoto = {
+                id: "7",
+                ...args.input
+            }
+        photos.push(newPhoto)
+        return newPhoto
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+const options = {
+    port: 4000, 
+    endpoint: '/graphql',
+    playground: '/playground'
+}
+
+const ready = ({port}) => 
+    console.log(`graph service running on port ${port}`);
+
+server.start(options,ready)
